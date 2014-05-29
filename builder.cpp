72005ccfa170f1defe2f406159364a1bda36e125
Morphei <morphei333@gmail.com>
2014-05-29 11:41:20 +0300

    Fixed small bugs

--- builder.cpp
+++ builder.cpp
@@ -55,5 +55,6 @@ if(dy<10) dy *= 10;
 for(int i = 0; height-height/20-dy*(i+1) > 0+height/15+2*dy; i++)
     drawingScene->addLine(7,height-height/20-dy*(i+1),13,height-height/20-dy*(i+1),QPen());
 
-QGraphicsTextItem *text = drawingScene->addText("© Г.Д. aka Morphei, 2014",QFont());
+QGraphicsTextItem *text = drawingScene->addText("© G.D. aka Morphei, 2014",QFont());
 }
+
